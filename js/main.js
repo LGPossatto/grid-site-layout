5723c8e50818697aca7be9da55ee32225f68c480
Luiz Gustavo <lgpossatto@gmail.com>
2021-02-16 17:38:59 -0300

    mod carousel js logic, bugs everywhere

--- js/main.js
+++ js/main.js
@@ -1,27 +1,61 @@
 // ------------------------------------------------------------------
 // carousel ---------------------------------------------------------
-let carouselBox = document.getElementById("carousel-box");
-let carouselBtnL = document.getElementById("carousel-btn-l");
-let carouselBtnR = document.getElementById("carousel-btn-r");
+const carouselBoxChildren = document.getElementById("carousel-box").children;
+const carouselBtnL = document.getElementById("carousel-btn-l");
+const carouselBtnR = document.getElementById("carousel-btn-r");
 
+console.log(carouselBoxChildren);
 const moveCarousel = (direction) => {
-  let carouselBoxPos = carouselBox.style.transform.slice(11, -3);
-
-  if (carouselBoxPos === "") {
-    carouselBoxPos = 0;
-  }
+  const pannelActive0 = document.querySelector(".active-0");
+  const pannelActive1 = document.querySelector(".active-1");
+  const pannelActive2 = document.querySelector(".active-2");
+  const pannelActive3 = document.querySelector(".active-3");
+  const pannelActive4 = document.querySelector(".active-4");
 
   if (direction === "left") {
-    carouselBox.style.transform = `translateX(${
-      270 + parseInt(carouselBoxPos)
-    }px)`;
-    console.log("ok l");
+    const carouselLength = carouselBoxChildren.length - 1;
+
+    for (let i = 0; i < carouselLength; i++) {
+      if (carouselBoxChildren[i].classList.contains("active-0")) {
+        carouselBoxChildren[i].classList.remove("active-0");
+        carouselBoxChildren[i].classList.add("active-1");
+        carouselBoxChildren[i + 1].classList.remove("active-1");
+        carouselBoxChildren[i + 1].classList.add("active-2");
+        carouselBoxChildren[i + 2].classList.remove("active-2");
+        carouselBoxChildren[i + 2].classList.add("active-3");
+        carouselBoxChildren[i + 3].classList.remove("active-3");
+        carouselBoxChildren[i + 3].classList.add("active-4");
+        carouselBoxChildren[i + 4].classList.remove("active-4");
+        carouselBoxChildren[i + 4].classList.add("hide-right");
+        carouselBoxChildren[carouselLength].classList.remove("hide-right");
+        carouselBoxChildren[carouselLength].classList.add("hide-left");
+      }
+      if (carouselBoxChildren[i].classList.contains("hide-left")) {
+        carouselBoxChildren[i].classList.remove("hide-left");
+        carouselBoxChildren[i].classList.add("active-0");
+      }
+    }
+    console.log(carouselBoxChildren);
   } else if (direction === "right") {
-    carouselBox.style.transform = `translateX(${
-      -270 + parseInt(carouselBoxPos)
-    }px)`;
-    console.log(`translateX(${-270 + parseInt(carouselBoxPos)}px)`);
-    console.log("ok r");
+    for (let i = 0; i < carouselBoxChildren.length; i++) {
+      if (carouselBoxChildren[i].classList.contains("active-4")) {
+        carouselBoxChildren[i + 1].classList.remove("hide-rigth");
+        carouselBoxChildren[i + 1].classList.add("active-4");
+        carouselBoxChildren[i].classList.remove("active-4");
+        carouselBoxChildren[i].classList.add("active-3");
+        carouselBoxChildren[i - 1].classList.remove("active-3");
+        carouselBoxChildren[i - 1].classList.add("active-2");
+        carouselBoxChildren[i - 2].classList.remove("active-2");
+        carouselBoxChildren[i - 2].classList.add("active-1");
+        carouselBoxChildren[i - 3].classList.remove("active-1");
+        carouselBoxChildren[i - 3].classList.add("active-0");
+        carouselBoxChildren[i - 4].classList.remove("active-0");
+        carouselBoxChildren[i - 4].classList.add("hide-left");
+        carouselBoxChildren[i - 5].classList.remove("hide-left");
+        carouselBoxChildren[i - 5].classList.add("hide-rigth");
+        break;
+      }
+    }
   }
 };
 
