98e65d77e8ba8d43c62fc7e2bd1a0592299865f3
Luiz Gustavo <lgpossatto@gmail.com>
2021-02-17 12:07:04 -0300

    js carousel working

--- js/main.js
+++ js/main.js
@@ -6,57 +6,60 @@ const carouselBtnR = document.getElementById("carousel-btn-r");
 
 console.log(carouselBoxChildren);
 const moveCarousel = (direction) => {
-  const pannelActive0 = document.querySelector(".active-0");
-  const pannelActive1 = document.querySelector(".active-1");
-  const pannelActive2 = document.querySelector(".active-2");
-  const pannelActive3 = document.querySelector(".active-3");
-  const pannelActive4 = document.querySelector(".active-4");
+  const carouselLastIndex = carouselBoxChildren.length - 1;
+
+  const getTrueIndex = (index) => {
+    if (index > carouselLastIndex) {
+      return index - carouselLastIndex - 1;
+    } else if (index < 0) {
+      return index + carouselLastIndex + 1;
+    } else {
+      return index;
+    }
+  };
 
   if (direction === "left") {
-    const carouselLength = carouselBoxChildren.length - 1;
-
-    for (let i = 0; i < carouselLength; i++) {
-      if (carouselBoxChildren[i].classList.contains("active-0")) {
-        carouselBoxChildren[i].classList.remove("active-0");
-        carouselBoxChildren[i].classList.add("active-1");
-        carouselBoxChildren[i + 1].classList.remove("active-1");
-        carouselBoxChildren[i + 1].classList.add("active-2");
-        carouselBoxChildren[i + 2].classList.remove("active-2");
-        carouselBoxChildren[i + 2].classList.add("active-3");
-        carouselBoxChildren[i + 3].classList.remove("active-3");
-        carouselBoxChildren[i + 3].classList.add("active-4");
-        carouselBoxChildren[i + 4].classList.remove("active-4");
-        carouselBoxChildren[i + 4].classList.add("hide-right");
-        carouselBoxChildren[carouselLength].classList.remove("hide-right");
-        carouselBoxChildren[carouselLength].classList.add("hide-left");
-      }
+    for (let i = 0; i <= carouselLastIndex; i++) {
       if (carouselBoxChildren[i].classList.contains("hide-left")) {
         carouselBoxChildren[i].classList.remove("hide-left");
-        carouselBoxChildren[i].classList.add("active-0");
+        carouselBoxChildren[i].classList.add("hide-right");
+        carouselBoxChildren[getTrueIndex(i + 1)].classList.remove("active-0");
+        carouselBoxChildren[getTrueIndex(i + 1)].classList.add("hide-left");
+        carouselBoxChildren[getTrueIndex(i + 2)].classList.remove("active-1");
+        carouselBoxChildren[getTrueIndex(i + 2)].classList.add("active-0");
+        carouselBoxChildren[getTrueIndex(i + 3)].classList.remove("active-2");
+        carouselBoxChildren[getTrueIndex(i + 3)].classList.add("active-1");
+        carouselBoxChildren[getTrueIndex(i + 4)].classList.remove("active-3");
+        carouselBoxChildren[getTrueIndex(i + 4)].classList.add("active-2");
+        carouselBoxChildren[getTrueIndex(i + 5)].classList.remove("active-4");
+        carouselBoxChildren[getTrueIndex(i + 5)].classList.add("active-3");
+        carouselBoxChildren[getTrueIndex(i + 6)].classList.remove("hide-right");
+        carouselBoxChildren[getTrueIndex(i + 6)].classList.add("active-4");
+        break;
       }
     }
-    console.log(carouselBoxChildren);
   } else if (direction === "right") {
-    for (let i = 0; i < carouselBoxChildren.length; i++) {
-      if (carouselBoxChildren[i].classList.contains("active-4")) {
-        carouselBoxChildren[i + 1].classList.remove("hide-rigth");
-        carouselBoxChildren[i + 1].classList.add("active-4");
-        carouselBoxChildren[i].classList.remove("active-4");
-        carouselBoxChildren[i].classList.add("active-3");
-        carouselBoxChildren[i - 1].classList.remove("active-3");
-        carouselBoxChildren[i - 1].classList.add("active-2");
-        carouselBoxChildren[i - 2].classList.remove("active-2");
-        carouselBoxChildren[i - 2].classList.add("active-1");
-        carouselBoxChildren[i - 3].classList.remove("active-1");
-        carouselBoxChildren[i - 3].classList.add("active-0");
-        carouselBoxChildren[i - 4].classList.remove("active-0");
-        carouselBoxChildren[i - 4].classList.add("hide-left");
-        carouselBoxChildren[i - 5].classList.remove("hide-left");
-        carouselBoxChildren[i - 5].classList.add("hide-rigth");
+    for (let i = 0; i <= carouselLastIndex; i++) {
+      if (carouselBoxChildren[i].classList.contains("hide-left")) {
+        carouselBoxChildren[getTrueIndex(i - 1)].classList.remove("hide-right");
+        carouselBoxChildren[getTrueIndex(i - 1)].classList.add("hide-left");
+        carouselBoxChildren[i].classList.remove("hide-left");
+        carouselBoxChildren[i].classList.add("active-0");
+        carouselBoxChildren[getTrueIndex(i + 1)].classList.remove("active-0");
+        carouselBoxChildren[getTrueIndex(i + 1)].classList.add("active-1");
+        carouselBoxChildren[getTrueIndex(i + 2)].classList.remove("active-1");
+        carouselBoxChildren[getTrueIndex(i + 2)].classList.add("active-2");
+        carouselBoxChildren[getTrueIndex(i + 3)].classList.remove("active-2");
+        carouselBoxChildren[getTrueIndex(i + 3)].classList.add("active-3");
+        carouselBoxChildren[getTrueIndex(i + 4)].classList.remove("active-3");
+        carouselBoxChildren[getTrueIndex(i + 4)].classList.add("active-4");
+        carouselBoxChildren[getTrueIndex(i + 5)].classList.remove("active-4");
+        carouselBoxChildren[getTrueIndex(i + 5)].classList.add("hide-right");
         break;
       }
     }
   }
+  console.log(carouselBoxChildren);
 };
 
 carouselBtnL.addEventListener("click", () => moveCarousel("left"));
